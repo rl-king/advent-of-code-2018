77cd3f0baacba07b66cd6bab58d75d3c04de346a
rl-king <hello@rubenlieking.com>
2018-12-04 21:06:22 +0100

    Refactor

---
+++ DayOne.hs
@@ -0,0 +1,23 @@
+module DayOne where
+
+
+main :: IO ()
+main = do
+  i <- lines <$> readFile "dayOne.txt"
+  print $ part1 (read . f <$> i)
+  where f ('+':xs) = xs
+        f xs = xs
+
+
+part1 :: [Int] -> Int
+part1 =
+  sum
+
+
+part2 :: [Int] -> Maybe Int
+part2 =
+  f (Nothing, []) . scanl1 (+) . concat . repeat
+  where f (Just x, _) _ = Just x
+        f (_, acc) (x:xs)
+          | x `elem` acc = f (Just x, acc) xs
+          | otherwise = f (Nothing, x:acc) xs
